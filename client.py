3e5d04d7512d4f5d4016a15a932bffdb91ec9011
Pronin Slava <pronin-v@bk.ru>
2023-01-27 12:15:35 +0300

    second

--- client.py
+++ client.py
@@ -1,5 +1,21 @@
+from pprint import pprint
+
 import requests
 
-data = requests.get('http://127.0.0.1:5000/hello/', json={'a':'b'}, params={'key':'value'})
+# data = requests.post('http://127.0.0.1:5000/adv/',
+#                      json={
+#                          'header': 'sdfg',
+#                          'description': 'gfhj',
+#                          'owner': 'Slava'
+#                      })
+# print(data.status_code)
+# print(data.text)
+
+
+# data = requests.get('http://127.0.0.1:5000/adv/Slava')
+# print(data.status_code)
+# pprint(data.text)
+
+data = requests.delete('http://127.0.0.1:5000/adv/3')
 print(data.status_code)
-print(data.text)
+pprint(data.text)
--- server.py
+++ server.py
@@ -12,29 +12,70 @@ Base = declarative_base()
 Session = sessionmaker(bind=engine)
 
 
+
 class Adv(Base):
+    __tablename__ = 'advs'
     id = Column(Integer, primary_key=True)
     header = Column(String(50), nullable=False)
     description = Column(String(50), nullable=False)
     date = Column(DateTime, server_default=func.now())
     owner = Column(String(50))
 
-
+Base.metadata.create_all(engine)
 
 
 class AdvView(MethodView):
-    def get(self):
-        pass
+    def get(self, name):
+        session = Session()
+        result = {}
+        adv_db = session.query(Adv).all()
+        list_user_db = [item.owner for item in adv_db]
+
+        if name not in list_user_db:
+            return jsonify({404: 'USER with name is not exist!'})
+
+        else:
+            result[name] = []
+            all_ = session.query(Adv).filter_by(owner=name).all()
+
+            for i in all_:
+                result[name].append({
+                    'id': i.id,
+                    'header': i.header,
+                    'description': i.description,
+                    'date': i.date.isoformat(),
+
+                })
+            return jsonify(result)
+
 
     def post(self):
-        pass
+        session = Session()
+        new_adv = Adv(**request.json)
+        session.add(new_adv)
+        session.commit()
+        return jsonify({
+            'id': new_adv.id,
+            'header': new_adv.header,
+            'description': new_adv.description,
+            'owner': new_adv.owner,
+        })
 
-    def patch(self):
-        pass
 
-    def delete(self):
-        pass
+    def delete(self, adv_id:int):
+        session = Session()
+        adv_del = session.query(Adv).get(adv_id)
+        if adv_del is None:
+            return jsonify({404:'ADV with id is not exist!!'})
+        session.delete(adv_del)
+        session.commit()
+        return jsonify({'status': 'success'})
 
-app.add_url_rule('/adv/', view_func=AdvView.as_view('advs'), methods=['GET', 'POST', 'PATCH', 'DELETE'])
+
+# app.add_url_rule('/adv/<name>/', view_func=AdvView.as_view('adv_get'), methods=['GET'])
+# app.add_url_rule('/adv/', view_func=AdvView.as_view('adv'), methods=['POST'])
+app.add_url_rule('/adv/<id>/', view_func=AdvView.as_view('adv_del'), methods=['DELETE'])
+
+
 
 app.run()
